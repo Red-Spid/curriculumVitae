3d4102da7abcffb5853f668c56d5da85e375463c
Red-Spid <2529638156@qq.com>
2020-10-07 20:28:52 +0800

    replace

--- js/index.js
+++ js/index.js
@@ -1,6 +1,17 @@
-
+var list, tar, url_;
 window.onload = function () {
-    var list = document.getElementsByClassName("list");
-    var tar = list[0].getElementsByTagName("ul")[0].getElementsByTagName("li");
+    list = document.getElementsByClassName("list");
+    tar = list[0].getElementsByTagName("ul")[0].getElementsByTagName("li");
     console.log(tar)
+    console.log(
+        window.location.pathname
+    )
+    url_ = window.location.pathname.split("?")
+    if (url_.length == 1) {
+        tar[url_.length - 1].setAttribute("class", 'active');
+    }
+
+}
+function replace() {
+    console.log('执行')
 }
