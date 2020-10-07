630c25e53117ed93dfe38c97872150eeb8ed3913
Red-Spid <2529638156@qq.com>
2020-10-07 20:04:29 +0800

    replace

---
+++ js/index.js
@@ -0,0 +1,6 @@
+
+window.onload = function () {
+    var list = document.getElementsByClassName("list");
+    var tar = list[0].getElementsByTagName("ul")[0].getElementsByTagName("li");
+    console.log(tar)
+}
