acc46630f9a21ada6d6db57e097b19851cf338ae
simon <67768597+Red-Spid@users.noreply.github.com>
2021-09-06 11:08:23 +0800

    Add files via upload

--- js/BasicSetting.js
+++ js/BasicSetting.js
@@ -1,1 +1,19 @@
 /* 基础设定 */
+
+const BASIC = {
+	domBasic:{
+		size: "12",
+		background:"",
+		fontWeight:"",
+		bold:"",
+		lineHeight:"",
+		color:""
+	},
+	game2048:{
+		start:"", //开始
+		end:"", //结束
+		delay:"", //延迟
+		background:[], //背景色
+	}
+	
+}
